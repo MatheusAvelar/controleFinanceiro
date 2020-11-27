b968a277cf1afa6da4e213087c27489be7f66648
Matheus Avelar <matheus_valladao@hotmail.com>
2020-11-27 14:42:30 -0300

    Retirado impressão da variavel teste

--- include_Redireciona.php
+++ include_Redireciona.php
@@ -1,6 +1,5 @@
 <?php
 $teste = session_status();
-print_r($teste);
 //Verifica se a Sessao ainda esta ativa para continuar e poder acessar outras URLs
 if ( session_status() == 'PHP_SESSION_DISABLED' ) {
     echo"<script>window.location.href='index.html';</script>";
